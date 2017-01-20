b4408a5ad66d43a1c391fa73d044d033f2528d7a
unknown <vc016505@SIE-018.rdg.ac.uk>
2017-01-20 10:59:03 +0000

    files

---
+++ ExampleGUI/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Comms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCon = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblRedStatus = new System.Windows.Forms.Label();
+            this.lblGreenStatus = new System.Windows.Forms.Label();
+            this.btnToggleRed = new System.Windows.Forms.Button();
+            this.btnToggleGreen = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPosLeft = new System.Windows.Forms.Label();
+            this.lblPosRight = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCon);
+            this.groupBox1.Controls.Add(this.txtIP);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(171, 127);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Communications";
+            // 
+            // btnCon
+            // 
+            this.btnCon.Location = new System.Drawing.Point(48, 80);
+            this.btnCon.Name = "btnCon";
+            this.btnCon.Size = new System.Drawing.Size(75, 23);
+            this.btnCon.TabIndex = 2;
+            this.btnCon.Text = "Connect";
+            this.btnCon.UseVisualStyleBackColor = true;
+            this.btnCon.Click += new System.EventHandler(this.btnCon_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(48, 30);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(98, 20);
+            this.txtIP.TabIndex = 1;
+            this.txtIP.Text = "10.215.2.36";
+            this.txtIP.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtIP_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblRedStatus);
+            this.groupBox2.Controls.Add(this.lblGreenStatus);
+            this.groupBox2.Controls.Add(this.btnToggleRed);
+            this.groupBox2.Controls.Add(this.btnToggleGreen);
+            this.groupBox2.Location = new System.Drawing.Point(189, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(295, 127);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "LEDs and buttons";
+            // 
+            // lblRedStatus
+            // 
+            this.lblRedStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRedStatus.Location = new System.Drawing.Point(166, 69);
+            this.lblRedStatus.Name = "lblRedStatus";
+            this.lblRedStatus.Size = new System.Drawing.Size(100, 18);
+            this.lblRedStatus.TabIndex = 1;
+            this.lblRedStatus.Text = "Red Status";
+            this.lblRedStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGreenStatus
+            // 
+            this.lblGreenStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblGreenStatus.Location = new System.Drawing.Point(166, 40);
+            this.lblGreenStatus.Name = "lblGreenStatus";
+            this.lblGreenStatus.Size = new System.Drawing.Size(100, 18);
+            this.lblGreenStatus.TabIndex = 1;
+            this.lblGreenStatus.Text = "Green Status";
+            this.lblGreenStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnToggleRed
+            // 
+            this.btnToggleRed.Location = new System.Drawing.Point(21, 67);
+            this.btnToggleRed.Name = "btnToggleRed";
+            this.btnToggleRed.Size = new System.Drawing.Size(108, 23);
+            this.btnToggleRed.TabIndex = 0;
+            this.btnToggleRed.Text = "Toggle Red";
+            this.btnToggleRed.UseVisualStyleBackColor = true;
+            this.btnToggleRed.Click += new System.EventHandler(this.btnToggleRed_Click);
+            // 
+            // btnToggleGreen
+            // 
+            this.btnToggleGreen.Location = new System.Drawing.Point(21, 38);
+            this.btnToggleGreen.Name = "btnToggleGreen";
+            this.btnToggleGreen.Size = new System.Drawing.Size(108, 23);
+            this.btnToggleGreen.TabIndex = 0;
+            this.btnToggleGreen.Text = "Toggle Green";
+            this.btnToggleGreen.UseVisualStyleBackColor = true;
+            this.btnToggleGreen.Click += new System.EventHandler(this.btnToggleGreen_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.lblPosRight);
+            this.groupBox3.Controls.Add(this.lblPosLeft);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(490, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(193, 127);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Tracks";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Starboard:";
+            // 
+            // lblPosLeft
+            // 
+            this.lblPosLeft.AutoSize = true;
+            this.lblPosLeft.Location = new System.Drawing.Point(84, 33);
+            this.lblPosLeft.Name = "lblPosLeft";
+            this.lblPosLeft.Size = new System.Drawing.Size(13, 13);
+            this.lblPosLeft.TabIndex = 0;
+            this.lblPosLeft.Text = "0";
+            // 
+            // lblPosRight
+            // 
+            this.lblPosRight.AutoSize = true;
+            this.lblPosRight.Location = new System.Drawing.Point(84, 72);
+            this.lblPosRight.Name = "lblPosRight";
+            this.lblPosRight.Size = new System.Drawing.Size(13, 13);
+            this.lblPosRight.TabIndex = 0;
+            this.lblPosRight.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 154);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "2nd Year Challenge - Example";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCon;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblRedStatus;
+        private System.Windows.Forms.Label lblGreenStatus;
+        private System.Windows.Forms.Button btnToggleRed;
+        private System.Windows.Forms.Button btnToggleGreen;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPosRight;
+        private System.Windows.Forms.Label lblPosLeft;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ExampleGUI/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using CommsLib;
+
+namespace Comms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            myClient = new TCPClient();
+            myClient.OnMessageReceived += new ClientBase.ClientMessageReceivedEvent(myClient_OnMessageReceived);
+
+            myRequestTimer = new Timer();
+            myRequestTimer.Interval = 500; //every half a second
+            myRequestTimer.Tick += new EventHandler(myRequestTimer_Tick);
+        }
+
+        TCPClient myClient;
+        private void btnCon_Click(object sender, EventArgs e)
+        {
+            if ("Connect" == btnCon.Text)
+            {
+                try
+                {
+                    myClient.ConnectToServer(txtIP.Text,9760);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message+"\r\n"+ex.StackTrace);
+                    return;
+                }
+
+                //if we get here, then we are connected
+                btnCon.Text="Disconnect";
+                
+                myRequestTimer.Start();
+            }
+            else
+            {
+                myRequestTimer.Stop();
+                myClient.Disconnect();
+                if (!myClient.isConnected) btnCon.Text="Connect";
+                
+
+            }
+        }
+
+        short leftPos, rightPos;
+        void myClient_OnMessageReceived(Client_Message_EventArgs e)
+        {
+            //we shall process the message here, e.RawMessage contains all bytes
+            // [0] is 255, [1] is length, [2] 255, [3] is cmd, [4-] are payload
+            if (e.RawMessage[3] == (byte)CommandID.GetLEDandSwitchStatus)
+            {
+                //we have received an LED status, so let's change labels accordingly
+                bool bGreenOn = (((e.RawMessage[4] & 0x10) ==0 ) ? true : false);
+                bool bRedOn =   (((e.RawMessage[4] & 0x20) ==0 ) ? true : false);
+
+                lblGreenStatus.BackColor = ((bGreenOn) ? Color.LimeGreen : SystemColors.ButtonFace);
+                lblRedStatus.BackColor = ((bRedOn) ? Color.Red : SystemColors.ButtonFace);
+            }
+
+            if (e.RawMessage[3] == (byte)CommandID.MotorPosition)
+            {
+                leftPos = (short)((uint)e.RawMessage[6] | ((uint)e.RawMessage[5] << 8));
+                rightPos = (short)((uint)e.RawMessage[9] | ((uint)e.RawMessage[8] << 8));
+
+            }
+        }
+
+
+
+        Timer myRequestTimer;
+        void myRequestTimer_Tick(object sender, EventArgs e)
+        {
+            if (!myClient.isConnected) return; //if no connection, don't do anything
+
+            //we will request the status of the LEDs on a regular basis
+            myClient.SendData(CommandID.GetLEDandSwitchStatus); //this type needs no payload
+            myClient.SendData(CommandID.MotorPosition);
+
+            lblPosLeft.Text = leftPos.ToString();
+            lblPosRight.Text = rightPos.ToString();
+        }
+
+
+        Boolean bGreenRequestOn = true;
+        Boolean bRedRequestOn = true;
+
+        private void btnToggleGreen_Click(object sender, EventArgs e)
+        {
+            if (!myClient.isConnected) return;
+
+            bGreenRequestOn = !bGreenRequestOn; //change from last time
+
+            int value = 0;
+            if (bGreenRequestOn) value |= 0x01;
+            if (bRedRequestOn) value |= 0x02;
+
+            myClient.SendData(CommandID.SetLEDs, new byte[] { (byte)value });
+        }
+
+        private void btnToggleRed_Click(object sender, EventArgs e)
+        {
+            if (!myClient.isConnected) return;
+
+            bRedRequestOn = !bRedRequestOn; //change from last time
+
+            int value = 0;
+            if (bGreenRequestOn) value |= 0x01;
+            if (bRedRequestOn) value |= 0x02;
+
+            myClient.SendData(CommandID.SetLEDs, new byte[] { (byte)value });
+        }
+
+
+        //Clean up our mess if user clicks the X button, hits AltF4 etc
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myRequestTimer.Stop(); //just in case ;-)
+            if (myClient.isConnected) myClient.Disconnect();
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+
+        }
+
+        bool robotIsMoving = false;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    myClient.SendData(CommandID.SetMotorsSpeed, new byte[] { 60, 60 });
+                    robotIsMoving = true;
+                    e.Handled=true;
+                    break;
+
+                case Keys.Down:
+                    myClient.SendData(CommandID.SetMotorsSpeed, new byte[] { 200, 200 });
+                    robotIsMoving = true;
+                    e.Handled=true;
+                    break;
+
+                case Keys.Left:
+                    myClient.SendData(CommandID.SetMotorsSpeed, new byte[] { 190, 60 });
+                    robotIsMoving = true;
+                    e.Handled=true;
+                    break;
+                case Keys.Right:
+                    myClient.SendData(CommandID.SetMotorsSpeed, new byte[] { 60, 190 });
+                    robotIsMoving = true;
+                    e.Handled=true;
+                    break;
+
+                default: e.Handled=false;
+                    break;
+
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (robotIsMoving)
+            {
+                myClient.SendData(CommandID.SetMotorsSpeed, new byte[] { 0,0 });
+                robotIsMoving = false;
+            }
+        }
+
+        private void txtIP_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                btnCon_Click(sender, null);
+                btnToggleGreen.Focus();
+            }
+        }
+    }
+}
